975714988036f37806b6b61c32597af9ef78d3f6
huytruong99th <huytruong99th@outlook.com>
2023-07-19 02:29:09 +0700

    heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors())
 app.use(express.json())
-app.use('/static', express.static(path.join(__dirname, 'build')))
+app.use(express.static('build'));
 
 app.get('/todos/:userEmail', async (req, res) => {
     const { userEmail } = req.params
