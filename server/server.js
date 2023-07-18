f4c8929d03da9a33a69abd61f05ab75fe05a2f08
huytruong99th <huytruong99th@outlook.com>
2023-07-19 02:19:40 +0700

    heroku testing

--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ const cors = require('cors');
 const { v4: uuidv4 } = require('uuid')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const path = require('path')
 
 const PORT = process.env.PORT || 8000;
 
 app.use(cors())
 app.use(express.json())
+app.use('/static', express.static(path.join(__dirname, 'build')))
 
 app.get('/todos/:userEmail', async (req, res) => {
     const { userEmail } = req.params
